93ed9aad11a2586e219f9bbb9b6c967112714253
Abrar101102 <abrarahemmad10@gmail.com>
2026-06-16 22:55:34 +0530

    Updated user from stock_user to postgres

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,7 @@ print(f"Found .env at: {dotenv_path}")
 
 password = "Abrar@1011"
 safe_password = urllib.parse.quote_plus(password)
-DATABASE_URL = f"postgresql://stock_user:{safe_password}@localhost:5432/stock_analyzer"
+DATABASE_URL = f"postgresql://postgres:{safe_password}@localhost:5432/stock_analyzer"
 ALPHA_VANTAGE_API_KEY = "JGPZOKCM2GN477A1"
 NEWS_API_KEY = os.getenv("NEWS_API_KEY", "")
 
